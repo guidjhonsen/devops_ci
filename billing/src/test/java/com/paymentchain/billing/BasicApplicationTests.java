3faf796b4f9c93a0e01129a7b51cf57ad1c9ed9b
Estanislao Ramos <guidjhonsen.tu.rey@gmail.com>
2025-12-31 23:26:17 -0400

    add test modificacion

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,16 +1,20 @@
 package com.paymentchain.billing;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 11";
-        assertNotNull(message);
+       String message = "Default Message Cambio Test Devops";
+       assertNotNull(message);//assertNotNull(message);
     }
 }
